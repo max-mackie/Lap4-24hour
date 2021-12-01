c06e6f294848819b845169fd28070af35a1106da
Max Mackie <80007111+max-mackie@users.noreply.github.com>
2021-12-01 16:50:12 +0000

    Delete form.py

--- form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-class UrlForm(FlaskForm):
-    url = StringField('URL', validators=[DataRequired(), Length(min=5, max=250)])
-    submit = SubmitField('Submit')
